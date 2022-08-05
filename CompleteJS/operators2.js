e0553fe4ea719e05004062d772451bca16ef5050
BhupeshXD <bhupeshdhapola7@gmail.com>
2022-08-06 00:35:21 +0530

    6aug

---
+++ CompleteJS/operators2.js
@@ -0,0 +1,10 @@
+// console.log(5+20);  // 25  + is a operator and 5 is operand
+
+// Assignment Operators
+// Arithmetic Operators
+// Comparison Operators
+// Logical Operators
+// String Operators
+// Conditional (ternary) Operators
+
+var 
